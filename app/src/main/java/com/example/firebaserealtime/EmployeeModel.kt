c56cbb736bb351eb6a21f4b4bf16f6fcc19d700d
gloryakinyi <akinyiglory2@gmail.com>
2023-06-16 08:38:16 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebaserealtime/EmployeeModel.kt
@@ -0,0 +1,8 @@
+package com.example.firebaserealtime
+
+data class EmployeeModel(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empAge: String? = null,
+    var empSalary: String? = null
+)
